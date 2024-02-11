2c4fdb059da2681dcf629778d058711d884da961
Marlon Gayle <marlon.gayle@live.com>
2024-02-11 15:29:08 -0500

    Update Program.cs
    add string.Empty to product Name property

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ app.Run();
 internal class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     public decimal Price { get; set; }
 }
